31bbd60f2b89bc9dda789286080fad5665f4ca9b
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-03-03 19:24:23 +0100

    Format.

--- src/models/_entities/unique_faces.rs
+++ src/models/_entities/unique_faces.rs
@@ -2,6 +2,7 @@
 
 use sea_orm::entity::prelude::*;
 use serde::{Deserialize, Serialize};
+
 #[derive(Clone, Debug, PartialEq, DeriveEntityModel, Serialize, Deserialize)]
 #[sea_orm(table_name = "unique_faces")]
 pub struct Model {
