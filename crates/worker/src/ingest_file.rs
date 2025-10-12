dd8494ed91762e0893ec0cc795e293469ca1cc3a
Ruurd Bijlsma <ruurd@bijlsma.dev>
2025-10-12 19:27:32 +0200

    Remove elapsed time measurements from ingest_file.rs

--- crates/worker/src/ingest_file.rs
+++ crates/worker/src/ingest_file.rs
@@ -5,7 +5,6 @@ use media_analyzer::MediaAnalyzer;
 use ruurd_photos_thumbnail_generation::generate_thumbnails;
 use sqlx::PgTransaction;
 use std::path::Path;
-use std::time::Instant;
 
 /// Processes a media file by generating thumbnails, analyzing its metadata, and storing the result.
 ///
@@ -26,7 +25,7 @@ pub async fn ingest_file(
     analyzer: &mut MediaAnalyzer,
     tx: &mut PgTransaction<'_>,
 ) -> color_eyre::Result<()> {
-    let now = Instant::now();
+    // Setup
     let thumb_config = get_thumb_options();
     let relative_path_str = relative_path_no_exist(file)?;
     let thumb_base_dir = thumbnails_dir();
@@ -39,19 +38,11 @@ pub async fn ingest_file(
         .expect("Thumb config heights should have at least one item.");
     let smallest_thumb_filename = format!("{smallest_thumb_size}p.avif");
     let tiny_thumb_path = thumbnail_out_dir.join(smallest_thumb_filename);
-    println!("setup: {:?}", now.elapsed());
 
-    let now = Instant::now();
+    // Generate thumb -> get media info -> store in db
     generate_thumbnails(file, &thumbnail_out_dir, &thumb_config).await?;
-    println!("generate_thumbnails: {:?}", now.elapsed());
-
-    let now = Instant::now();
     let media_info = analyzer.analyze_media(file, &tiny_thumb_path).await?;
-    println!("analyze_media: {:?}", now.elapsed());
-
-    let now = Instant::now();
     store_media_item(tx, &relative_path_str, &media_info, &media_item_id, user_id).await?;
-    println!("store_media_item: {:?}", now.elapsed());
 
     Ok(())
 }
